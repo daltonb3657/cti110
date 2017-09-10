80649b39a7daec88c8e2ca0862dd578652083340
daltonb3657 <31832007+daltonb3657@users.noreply.github.com>
2017-09-10 18:56:31 -0400

    Add files via upload
    First Submission

---
+++ M2T1_SalesPrediction_Dalton.py
@@ -0,0 +1,8 @@
+# CTI-110
+# M2T1 - Sales Prediction
+# Brandon Dalton
+# 9/10/2017
+#
+total_sales = float(input('Enter the projected sales: '))
+profit = total_sales * 0.23
+print('The profit is $', format(profit, ',.2f'))
