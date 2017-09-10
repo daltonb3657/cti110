1ac4f280425c78636ce206798199a05232ce4b3b
daltonb3657 <31832007+daltonb3657@users.noreply.github.com>
2017-09-10 19:16:10 -0400

    Add files via upload
    First submission

---
+++ M2HW1_DistanceTraveled_BrandonDalton.py
@@ -0,0 +1,12 @@
+# CTI-110
+# M2HW1 - Distance Traveled
+# Brandon Dalton
+# 9/10/2017
+#
+speed = 70
+distanceAfter6 = speed * 6
+distanceAfter10 = speed * 10
+distanceAfter15 = speed * 15
+print('The distance the car will travel in 6 hours is', distanceAfter6, 'miles')
+print('The distance the car will travel in 10 hours is', distanceAfter10, 'miles')
+print('The distance the car will travel in 15 hours is', distanceAfter15, 'miles')
